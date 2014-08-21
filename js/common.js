1774ea56020995eee2d747c07d90e407d27eadbf
buschidos <buschidos@buschidos.local>
2014-08-21 18:10:46 +0300

    device page and mobile

--- js/common.js
+++ js/common.js
@@ -1,8 +1,27 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
+	var    navItem = $('.js-nav'),
+			subNav = navItem.find('.js-subnav'),
+		menuButton = $('.js-show-menu'),
+		menuToShow = $('.js-menu-to-show');
+
+	subNav.hide();
+
+	navItem.click(function(event) {
+		subNav.slideToggle();
+	});
+
+	// show-hide menu on mobile
+	menuButton.click(function(event) {
+		$(this).toggleClass('is-active');
+		menuToShow.slideToggle();
+		subNav.slideUp();
+	});
+
+	// $(document).click(function(event) {
+	// 	if ($(event.target).closest('.js-subnav').length == 0) {
+	// 		subNav.slideUp();
+	// 	};
 	// });
 
-	console.log($('body').html());
 });
